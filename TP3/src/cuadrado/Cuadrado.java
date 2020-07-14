86686ba9332cd8f5f88127aedd84b3ed35d9e33b
Mac-Kett <Mac-Kett@users.noreply.github.com>
2020-07-13 23:56:47 -0300

    Clase cuadrado con su test

---
+++ TP3/src/cuadrado/Cuadrado.java
@@ -0,0 +1,43 @@
+package ejercicio2;
+
+public class Cuadrado {
+
+	private int lado;
+
+	public Cuadrado(int lado) {
+		setLado(lado);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	public int getLado()) {
+		return lado;
+	}
+
+	public void setLado(int lado) {
+		this.lado = lado;
+	}
+
+	public int perimetro() {
+		int  aux;
+		aux = this.lado * 4;
+		return aux;
+	}
+	
+	public int superficie() {
+		int aux;
+		aux = this.lado * this.lado;
+		return aux;
+	}
+
+	@Override
+	public String toString() {
+		return "Cuadrado [lado=" + lado + ", getLado()=" + getLado() + ", perimetro()=" + perimetro()
+				+ ", superficie()=" + superficie() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+}
