dbb14b6456f76d328fb9b08dcd4c8ef262ff0c22
Mac-Kett <Mac-Kett@users.noreply.github.com>
2020-08-05 11:12:58 -0300

    Definición de clases y atributos

---
+++ TP3/src/concesionaria/Concesionaria.java
@@ -0,0 +1,24 @@
+package concesionaria;
+
+public class Concesionaria {
+
+	private String nombre;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
---
+++ TP3/src/concesionaria/Persona.java
@@ -0,0 +1,29 @@
+package concesionaria;
+
+public class Persona {
+
+	private String dni;
+	private String fechaNacimiento;
+	
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
