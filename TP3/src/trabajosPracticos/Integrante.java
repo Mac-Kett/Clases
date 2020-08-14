8cdfbc11e0b153f1ced08358a664e2049b1b8955
Mac-Kett <Mac-Kett@users.noreply.github.com>
2020-08-13 22:13:39 -0300

    first commit

---
+++ TP3/src/trabajosPracticos/Integrante.java
@@ -0,0 +1,13 @@
+package trabajosPracticos;
+
+public class Integrante {
+
+	private String nombre;
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	
+}
