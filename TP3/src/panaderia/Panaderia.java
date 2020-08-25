93cf0d53535f98de4f456b34a4002783a11c12ce
Mac-Kett <Mac-Kett@users.noreply.github.com>
2020-08-25 13:01:25 -0300

    First commit

---
+++ TP3/src/panaderia/Panaderia.java
@@ -0,0 +1,18 @@
+package panaderia;
+
+public class Panaderia {
+
+	private String producto;
+	private int precio;
+	
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		int numTicket = 1;
+		
+		while (numTicket == 0) {
+			
+		}
+	}
+
+}
