398046d1b523549d1c8d4e2e473b13da2de11442
Mac-Kett <Mac-Kett@users.noreply.github.com>
2020-09-19 16:25:00 -0300

    First Commit

---
+++ TP3/src/gastos_anuales/GastosAnuales.java
@@ -0,0 +1,28 @@
+package gastos_anuales;
+
+import java.util.ArrayList;
+
+public class GastosAnuales {
+
+	private ArrayList <Rubro> rubros;
+	
+	//condtructor
+	
+	public void agregarGasto(Mes mes, String, double monto){
+		
+	}
+	
+	private Rubro obtenerRubro(String ****) {
+		
+	}
+	
+	private Rubro buscarRubro(String ***) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
---
+++ TP3/src/gastos_anuales/Mes.java
@@ -0,0 +1,5 @@
+package gastos_anuales;
+
+public enum Mes {
+ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+}
