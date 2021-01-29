e9cb72ee117c87b43396acd3b85d1181406f8c32
Mac-Kett <Mac-Kett@users.noreply.github.com>
2021-01-29 19:01:38 -0300

    Getters, setters y métodos a resolver

--- TallerProgramacion1/src/EmpresaFruticola/Plantacion.java
+++ TallerProgramacion1/src/EmpresaFruticola/Plantacion.java
@@ -10,7 +10,57 @@ public class Plantacion {
 		setNombre(nom);
 		setHectareas(hec);
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getHectareas() {
+		return hectareas;
+	}
+
+	private void setHectareas(double hectareas) {
+		this.hectareas = hectareas;
+	}
 	
+	public void agregarCosecha(Cosecha c) {
+		//TODO
+	}
 	
+	public void mostrarDatos() {
+		//TODO
+	}
+	
+	public void mostrarTodos() {
+		//TODO
+	}
+	
+	public void rendimientoMaximo() {
+		//TODO
+	}
+	
+	public void rendimientoMinimo() {
+		//TODO
+	}
+	
+	public void mostrarMenoresA (int num) {
+		//TODO
+	}
+
+	public void mostrarMayoresA(int num) {
+		//TODO
+	}
+	
+	public Cosecha getCosechaPorNumero(int num) {
+		//TODO
+	}
+	
+	public Cosecha getCosechaPorProducto(Producto prod) {
+		//TODO
+	}
 	
 }
