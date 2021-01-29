a78c4745aa7bedf956066425ceab2cd70e95cfd9
Mac-Kett <Mac-Kett@users.noreply.github.com>
2021-01-29 18:49:36 -0300

    First Commit

---
+++ TallerProgramacion1/src/EmpresaFruticola/EmpresaFruticola.java
@@ -0,0 +1,18 @@
+package EmpresaFruticola;
+
+public class EmpresaFruticola {
+
+	private String nombre;
+	private ArrayList<DatosCosecha> datosCosecha;
+	private ArrayList<Plantacion> plantaciones;
+	private int[][] contCosechas;
+	private double[][] toneladasAcumuladas;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
