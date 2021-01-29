9dbe64941ce2e4e7485a75727ad3962f157ba440
Mac-Kett <Mac-Kett@users.noreply.github.com>
2021-01-29 18:56:04 -0300

    Cargados los métodos a resolver

--- TallerProgramacion1/src/EmpresaFruticola/EmpresaFruticola.java
+++ TallerProgramacion1/src/EmpresaFruticola/EmpresaFruticola.java
@@ -28,6 +28,29 @@ public class EmpresaFruticola {
 		//TODO
 	}
 
+	public void cargarDatosCosecha (int a, int b, Producto p, double num, double num2) {
+		//TODO
+	}
+	
+	private boolean nroDePlantacionValido(int num) {
+		//TODO
+	}
+	
+	private int cantidadDeProductos() {
+		//TODO
+	}
+	
+	public void procesarDatos() {
+		//TODO
+	}
+	
+	public void listarDatosEntrada() {
+		//TODO
+	}
+	
+	public void listarDatosSalida() {
+		//TODO
+	}
 	
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
