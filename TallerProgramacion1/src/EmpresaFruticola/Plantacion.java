0b32baf3b40e9196c7c17464e5c4fd5f30e345a3
Mac-Kett <Mac-Kett@users.noreply.github.com>
2021-01-29 18:58:05 -0300

    Construcción de la clase con atributos

---
+++ TallerProgramacion1/src/EmpresaFruticola/Plantacion.java
@@ -0,0 +1,16 @@
+package EmpresaFruticola;
+
+public class Plantacion {
+
+	private String nombre;
+	private double hectareas;
+	private ArrayList<Cosecha> cosechas;
+	
+	public Plantacion(String nom, double hec) {
+		setNombre(nom);
+		setHectareas(hec);
+	}
+	
+	
+	
+}
