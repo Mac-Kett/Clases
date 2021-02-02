705471f09cbf8145aad93515bc2e3ca916dd8f38
Mac-Kett <Mac-Kett@users.noreply.github.com>
2021-02-02 18:59:37 -0300

    Excepciones de Constructor

--- TallerProgramacion1/src/EmpresaFruticola/DatosCosecha.java
+++ TallerProgramacion1/src/EmpresaFruticola/DatosCosecha.java
@@ -9,11 +9,36 @@ public class DatosCosecha {
 	private double hectareas;
 	
 	public DatosCosecha(int nplan, int ncos, Producto p, double ton, double hec) {
+		
+		if(nplan <=0) {
+			throw new IllegalArgumentException ("El número de plantación no puede ser cero o negativo");
+		} else {
 		setNroPlantacion(nplan);
+		}
+		
+		if(ncos <= 0) {
+			throw new IllegalArgumentException("El número de Cosecha no puede ser cero o negativo");
+		} else {
 		setNroCosecha(ncos);
+		}
+		
+		try {
 		setProducto(p);
+		} catch (NullPointerException npe) {
+			System.out.println(" El producto no puede ser nulo");
+		}
+		
+		if(ton <= 0) {
+		throw new IllegalArgumentException ("La cantidad de toneladas no puede ser cero o negativo");
+		} else {
 		setToneladas(ton);
+		}
+		
+		if(hec <= 0) {
+			throw new IllegalArgumentException ("Las hectareas no pueden ser cero o negativo");
+		} else {
 		setHectareas(hec);
+		}
 	}
 
 	public int getNroPlantacion() {
